9854b94b61efc8c5e5f4baa3191d261815ed9adf
mamede_ticv <gabriel.mamede@cidadeviva.org>
2025-03-18 01:23:18 -0300

    Tipos e Variáveis

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class TiposVariaveis {
+    public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 58102149;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salario = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+    }
 }
