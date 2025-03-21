b6953f8aeb5b8aaa302ff65a3d5b24bd56f5aec4
gabrielmamedee <mamede.dev@gmail.com>
2025-03-20 23:08:47 -0300

    Java Doc

---
+++ src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * <h1>Calculadora<h1/>
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:<b/> Leia atentamente a documentação desta classes
+ * para desfrutar dos recursos oferecidos pelo autor
+ *
+ * @author gabrielmamede
+ * @version 1.0
+ * @since 20/03/2025
+ */
+public class Calculadora {
+    /**
+     * Este metodo é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do metodo
+     * @param numeroDois este é o segundo parâmetro do metodo
+     * @return int o resultado deste metodo é a soma dos dois números
+     */
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+}
---
+++ src/Comentarios.java
@@ -0,0 +1,21 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário numa única linha
+
+        /* Olá
+         * Eu sou um comentário
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+    }
+    /**
+     * Esta duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação
+     * Que incrível!!!
+     */
+    public void metodo(){
+
+    }
+}
