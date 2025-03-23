4a219fcadc568e9ff4747d07af45bc138cdb06e4
gabrielmamedee <mamede.dev@gmail.com>
2025-03-23 18:18:20 -0300

    estruturas condicionais

---
+++ src/controle_de_fluxo/ResultadoEscolar.java
@@ -0,0 +1,17 @@
+package controle_de_fluxo;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+        int nota = 8;
+
+        if(nota >= 7)
+            System.out.println("Aprovado");
+        else
+            System.out.println("Reprovado");
+
+        //Condição ternária
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
---
+++ src/controle_de_fluxo/ResultadoFinalEscolar.java
@@ -0,0 +1,18 @@
+package controle_de_fluxo;
+
+public class ResultadoFinalEscolar {
+    public static void main(String[] args) {
+        int nota = 7;
+
+        if(nota >= 7)
+            System.out.println("Aprovado!!");
+        else if(nota >= 5 && nota < 7)
+            System.out.println("Recuperação");
+        else
+            System.out.println("Reprovado");
+
+        //Condição ternária
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
