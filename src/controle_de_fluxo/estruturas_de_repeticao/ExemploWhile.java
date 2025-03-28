3e9a838875ae17de75e1f327b7fa9bfce7011148
gabrielmamedee <mamede.dev@gmail.com>
2025-03-27 22:06:01 -0300

    Estruturas de Repetição

--- src/controle_de_fluxo/estruturas_de_repeticao/ExemploWhile.java
+++ src/controle_de_fluxo/estruturas_de_repeticao/ExemploWhile.java
@@ -7,11 +7,17 @@ public class ExemploWhile {
 
         while (mesada > 0) {
             Double valorDoce = valorAleatorio();
+
             if (valorDoce > mesada)
                 valorDoce = mesada;
+
+            System.out.println("Doce do valor: " + valorDoce + "Adicionado no carrinho");
+            mesada = mesada - valorDoce;
         }
+        System.out.println("Mesada:" + mesada);
+        System.out.println("Joãozinho gastou toda a sua mesada em doces");
     }
     private static double valorAleatorio() {
-        return ThreadLocalRandom.current().nextDouble(2, 8);
+        return ThreadLocalRandom.current().nextDouble(2, 15);
     }
 }
